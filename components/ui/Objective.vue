c63b5c59d2b1277e659509c610792dfa3684b3fc
Dramane-dev <dramane.dev@gmail.com>
2024-05-03 01:37:52 +0200

    feat(pages/index): use RedisService to get email counter from redis

--- components/ui/Objective.vue
+++ components/ui/Objective.vue
@@ -1,9 +1,17 @@
+<script setup lang="ts">
+
+defineProps<{
+  objectiveValue: Ref<number>
+}>()
+
+</script>
+
 <template>
   <p
     class="home__item_left_effect mb-5 mt-1 font-syne text-[18px] text-red-brown opacity-0"
   >
     Déjà
-    <span class="mx-1 inline-block text-3xl text-gold">134</span>
+    <span class="mx-1 inline-block text-3xl text-gold">{{ objectiveValue }}</span>
     inscrits. Objectif <span class="underline">2000</span> !
   </p>
 </template>
